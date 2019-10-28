6b4449699083ca0e9ddae45dd371668870eab4b3
emamare <martin.marecic@ericsson.com>
2019-10-28 11:03:50 +0100

    Small cosmetic changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import './index.css';
+import { createLogger } from 'redux-logger';
+import 'tachyons';
+
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { searchRobots, requestRobots } from './reducers';
-import 'tachyons';
+import { requestRobots, searchRobots } from './reducers';
+
+import './index.css';
 
 const logger = createLogger();
 
-const rootReducer = combineReducers({ searchRobots, requestRobots })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const rootReducers = combineReducers({requestRobots, searchRobots});
 
-ReactDOM.render(<Provider store={store}>
-                    <App/>
-                </Provider>, document.getElementById('root'));
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers.js
+++ src/reducers.js
@@ -6,7 +6,7 @@ import {
 } from './constants';
 
 const initialStateSearch = {
-    searchField : ''
+    searchField: ''
 };
 
 const initialStateRobots = {
