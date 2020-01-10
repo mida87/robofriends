1035fd1716ee66283631ba7c4d5bfc6d725c62d0
emamare <martin.marecic@ericsson.com>
2020-01-10 10:24:51 +0100

    Updates

---
+++ precache-manifest.9de4c888f9f67f72b31b4f5c53eab437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d313cbdfb4304cce10d9af4a9edbf1e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecd7de5395cb4490a77",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "32067245ee00dfb84e10",
+    "url": "/static/css/main.aff594dc.chunk.css"
+  },
+  {
+    "revision": "0ecd7de5395cb4490a77",
+    "url": "/static/js/2.4bdd5cbe.chunk.js"
+  },
+  {
+    "revision": "32067245ee00dfb84e10",
+    "url": "/static/js/main.4c5021e5.chunk.js"
+  },
+  {
+    "revision": "b0a546bcc125fb21d6be",
+    "url": "/static/js/runtime~main.75271b63.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
